ccd46dc4f4d59bbaa47ee3c98c8f2318d10742f6
Richard Simpson <richardsimpson@outlook.com>
2023-03-07 13:32:02 -0600

    feat: projen project

---
+++ .projenrc.js
@@ -0,0 +1,42 @@
+const projen = require("projen");
+const {
+  awscdk: { AwsCdkConstructLibrary },
+  javascript: { NodePackageManager },
+} = projen;
+
+const project = new AwsCdkConstructLibrary({
+  author: "Richard Simpson",
+  authorAddress: "richardsimpson@outlook.com",
+  defaultReleaseBranch: "main",
+  name: "@richicoder/elastic-serverless-forwarder-cdk",
+  repositoryUrl:
+    "git@github.com:RichiCoder1/elastic-serverless-forwarder-cdk.git",
+  catalog: {
+    twitter: "RichiCoder1",
+    announce: true,
+  },
+
+  cdkVersion: "2.66.0",
+  stability: "experimental",
+
+  minNodeVersion: "18.0.0",
+  packageManager: NodePackageManager.NPM,
+  prettier: true,
+  renovatebot: true,
+  autoApproveOptions: {
+    allowedUsernames: ["renovate[bot]"],
+  },
+  docgen: true,
+  stale: true,
+  compat: true,
+
+  githubOptions: {
+    mergify: false,
+  },
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
